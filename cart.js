34f31838602d5b9a5504137cdcaee854446bff28
saumittras <saumittra.eee@gmail.com>
2024-10-12 13:02:24 +0600

    added  local Storage function

--- cart.js
+++ cart.js
@@ -3,17 +3,47 @@ const addProduct = () => {
   const productQuantity = document.getElementById("product-quantity");
   const product = productName.value;
   const quantity = productQuantity.value;
-  console.log(product, quantity);
   productName.value = "";
   productQuantity.value = "";
 
-  localStorage.setItem(product, quantity);
-  uldispaly(product, quantity);
+  dispalyItem(product, quantity);
+  saveProductToLocalStorage(product, quantity);
 };
 
-const uldispaly = (product, quentity) => {
+const getShoppingCart = () => {
+  const storedCart = localStorage.getItem("cart");
+  let cart = {};
+  if (storedCart) {
+    cart = JSON.parse(storedCart);
+  }
+  return cart;
+};
+
+const dispalyItem = (product, quentity) => {
   const ul = document.getElementById("ul-container");
   const li = document.createElement("li");
   li.innerText = `${product}: ${quentity}`;
   ul.append(li);
 };
+
+const saveProductToLocalStorage = (product, quantity) => {
+  const cart = getShoppingCart();
+  console.log(cart);
+  cart[product] = quantity;
+  console.log(cart);
+  const cartStringifyed = JSON.stringify(cart);
+  console.log(cartStringifyed);
+  localStorage.setItem("cart", cartStringifyed);
+};
+
+const displayProductsfromLocalStorage = () => {
+  const saveCart = getShoppingCart();
+  console.log(saveCart);
+  for (const product in saveCart) {
+    const quantity = saveCart[product];
+    console.log(product, quantity);
+    dispalyItem(product, quantity);
+  }
+};
+
+displayProductsfromLocalStorage();
